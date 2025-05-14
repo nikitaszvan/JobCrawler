063f2656825ee7a03111ceca9f48b3c27f41b4af
Nikita Van <138067509+nikitaszvan@users.noreply.github.com>
2025-05-13 20:33:17 -0400

    Update exception.py

--- jobcrawler/exception.py
+++ jobcrawler/exception.py
@@ -1,5 +1,5 @@
 """
-jobspy.jobboard.exceptions
+jobcrawler.jobboard.exceptions
 ~~~~~~~~~~~~~~~~~~~
 
 This module contains the set of Scrapers' exceptions.
@@ -10,31 +10,14 @@ class LinkedInException(Exception):
     def __init__(self, message=None):
         super().__init__(message or "An error occurred with LinkedIn")
 
-
 class IndeedException(Exception):
     def __init__(self, message=None):
         super().__init__(message or "An error occurred with Indeed")
-
-
-class ZipRecruiterException(Exception):
-    def __init__(self, message=None):
-        super().__init__(message or "An error occurred with ZipRecruiter")
-
 
 class GlassdoorException(Exception):
     def __init__(self, message=None):
         super().__init__(message or "An error occurred with Glassdoor")
 
-
 class GoogleJobsException(Exception):
     def __init__(self, message=None):
         super().__init__(message or "An error occurred with Google Jobs")
-
-
-class BaytException(Exception):
-    def __init__(self, message=None):
-        super().__init__(message or "An error occurred with Bayt")
-
-class NaukriException(Exception):
-    def __init__(self,message=None):
-        super().__init__(message or "An error occurred with Naukri")
